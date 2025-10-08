05149a360b93b2e15711dc71a29feade25fb92e7
Paras <jadhavparas740@gmail.com>
2025-10-08 15:00:22 +0530

    function

--- Data types/Function.py
+++ Data types/Function.py
@@ -16,3 +16,27 @@ def my_function(fname):
 my_function('paras')
 my_function('nishant')
 my_function('sona')
+
+
+
+
+# Function Paasing a list as argument
+print('\n Function Paasing a list as argument')
+def my_function2(food):
+    for x in food:
+        print(x)
+
+fruits = ['apple', 'banana', 'chiku']
+
+my_function2(fruits)
+
+
+# Return Values
+print('\n Return Values')
+
+def my_function3(x):
+    return 5 * x
+
+print(my_function3(3))
+print(my_function3(5))
+print(my_function3(9))
